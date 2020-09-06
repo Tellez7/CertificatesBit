b4819d5f6c5b840c247e31b715d7d5c26b42678e
Esteban Tellez <es-tellez@hotmail.com>
2020-09-06 02:55:43 -0500

    Creacion del proyecto

---
+++ CertificatesBit/src/main/java/com/bit/certificates/CertificatesBitApplication.java
@@ -0,0 +1,13 @@
+package com.bit.certificates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificatesBitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificatesBitApplication.class, args);
+	}
+
+}
---
+++ CertificatesBit/src/test/java/com/bit/certificates/CertificatesBitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bit.certificates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificatesBitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
